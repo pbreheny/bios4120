b7ea5fb8cca8313470a4b0490d816f2afffd73b1
Patrick Breheny <patrick-breheny@uiowa.edu>
2017-02-22 12:06:55 -0600

    Updated documentation

--- R/normal.R
+++ R/normal.R
@@ -7,18 +7,18 @@ normal <- function() {
   cut3 <- round(100*runif(1))
   s1 <- sample(1:2, 1)
   s2 <- sample(1:2, 1)
-  
+
   out("Suppose that, for individuals in a certain population, levels of ", mark$marker, " are normally distributed with a mean of ", m, " and a standard deviation of ", SD, ".")
   out("a) What percent of individuals have ", mark$marker," levels ", c("below ", "above ")[s1], cut1, "?")
   out("b) What percent of individuals have ", mark$marker," levels ", c("within ", "more than ")[s2], cut2, c(" of"," away from")[s2], " the mean?")
   out("c) What is the ", cut3, "th percentile of ", mark$marker, " levels in this population?")
-  
+
   cat("\n\n")
   readline("Press <Enter> to reveal answers")
   cat("\n\n")
-  
+
   alpha <- 2*pnorm(-cut2/SD)
-  out("a) ", round(100*pnorm(cut1, m, SD, lower=(s1==1)), 1))
+  out("a) ", round(100*pnorm(cut1, m, SD, lower.tail=(s1==1)), 1))
   out("b) ", round(100*ifelse(s2==1, 1-alpha, alpha), 1))
   out("c) ", round(qnorm(cut3/100, m, SD), 1))
 }
---
+++ inst/tests/test.R
@@ -0,0 +1,21 @@
+# Quiz 2
+prob()  ## Probability question
+corr()  ## Correlation/regression question
+quiz2() ## Randomly draws one of the above two questions
+
+# Quiz 3
+distr() ## Distributions (normal, binomial, central limit theorem)
+cat1()  ## One-sample categorical data (using CLT approximation)
+cont1() ## One-sample continuous data
+quiz3() ## Randomly draws one of the above three questions
+
+# Quiz 4
+cat2()  ## Two-sample categorical data
+cont2() ## Two-sample continuous data
+quiz4() ## Randomly draws one of the above two questions
+
+# Post-Quiz 4
+ANOVA() ## ANOVA/multiple comparison question 
+surv()  ## Kaplan-Meier/survival question 
+quiz5() ## Randomly draws one of the above two questions 
+final() ## Randomly draws a question from quiz2, quiz3, quiz4, or quiz5 
